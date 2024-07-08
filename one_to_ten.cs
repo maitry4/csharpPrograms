2b33b4b5154e598bbf151ac5c4a5b4c302b5c7db
Maitry Parikh <maitryparikh23@gmail.com>
2024-07-08 08:14:49 +0530

    Create one_to_ten.cs

---
+++ one_to_ten.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Print Numbers from 1 to 10
+namespace _404print_one_to_ten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 0; i < 11; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
