3263bae4c5dd84766102624dc67c66a557c48423
Maitry Parikh <maitryparikh23@gmail.com>
2024-07-08 08:02:38 +0530

    Create swap.cs

---
+++ swap.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Swap Two Numbers
+namespace _15612422404
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            int c;
+            c = a;
+            a = b;
+            b = c;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.ReadLine();
+        }
+    }
+}
