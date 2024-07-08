d4884fbe2144b111907ef415d2ab113bf9149604
Maitry Parikh <maitryparikh23@gmail.com>
2024-07-08 08:35:01 +0530

    Create odd_even.cs

---
+++ odd_even.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _404EvenOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 21;
+            if(a%2 == 0)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+            Console.ReadLine();
+        }
+    }
+}
